ff723aaccb1cb11d784be19d2c48eb5d7bad0264
Aiden F <aidenfavish@gmail.com>
2022-12-11 12:02:16 -0800

    Control panel up and running, grand message log now reporting

---
+++ commands.py
@@ -0,0 +1,66 @@
+import json
+import discord
+import datetime
+
+async def custom_commands(message, client):
+    msg = message.content.lower()
+    with open("storage.json", "r") as j:
+        data = json.load(j)
+
+    if msg in data["Commands"]:
+        await message.channel.send(data["Commands"][msg])
+
+
+async def statistics(client):
+    counterC = client.get_channel(958155533955780659)
+    with open("storage.json", "r") as j:
+        data = json.load(j)
+    messageBoard = counterC.get_partial_message("1051588395165548544")
+    display = ""
+
+    block1 = "```yaml\n" + str(data["MainCounter"]) + " total server messages (Not including bots)```"
+    block2 = "```fix"
+    block4 = "```ini\n[ Last updated: " + str(datetime.datetime.now()) + " ]```"
+
+    memoryDict = {}
+    while data["MsgCounter"]:
+        holder = [0, 0]
+        for i in data["MsgCounter"]:
+            if data["MsgCounter"][i] >= holder[1]:
+                holder = [i, data["MsgCounter"][i]]
+        memoryDict[holder[0]] = holder[1]
+        del data["MsgCounter"][holder[0]]
+    data["MsgCounter"] = memoryDict
+    counter = 1
+    for i in data["MsgCounter"]:
+        user = client.get_user(int(i))
+        if user == None:
+            user = client.user
+        block2 += "\n" + str(counter) + ". " + user.name + " -> " + str(data["MsgCounter"][i]) + " messages"
+        counter += 1
+    block2 += "```"
+    display = block1 + block2 + block4
+    await messageBoard.edit(content=display)
+
+
+async def log(message, client):
+    gml = client.get_channel(955166586090696734)
+    author = str(message.author.id)
+    with open("storage.json", "r") as j:
+        data = json.load(j)
+
+    embed = discord.Embed(title=message.author.name + "#" + message.author.discriminator, url=message.jump_url,
+                          description=message.content, color=message.author.top_role.color)
+    await gml.send(embed=embed)
+    data["MainCounter"] += 1
+
+    # Updates the counters by author and by server
+    if author not in data["MsgCounter"]:
+        data["MsgCounter"][author] = 1
+    else:
+        data["MsgCounter"][author] += 1
+
+    with open("storage.json", "w") as j:
+        json.dump(data, j)
+
+    await statistics(client)
--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import random
 import discord
 from HiddenData import token
 import json
-import asyncio
+import commands
 
 intents = discord.Intents.default()
 intents.members = True
 intents.presences = True
+intents.message_content = True
+intents.invites = True
 client = discord.Client(intents=intents)
+
 
 @client.event
 async def on_ready():
@@ -18,3 +21,17 @@ async def on_ready():
         data = json.load(j)
     activity = discord.Activity(type=discord.ActivityType.watching, name=" " + data["Status"])
     await client.change_presence(status=discord.Status.online, activity=activity)
+
+
+@client.event
+async def on_message(message):
+    # Makes sure Jason doesn't read his own messages
+    if message.author.id != client.user.id:
+        await commands.custom_commands(message, client)
+
+        # Blacklists testing channel to prevent diluting of the grand message log
+        if message.channel.id != 948350259896143883:
+            await commands.log(message, client)
+
+
+client.run(token)
